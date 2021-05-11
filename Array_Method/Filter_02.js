1394f0914c2217a146f669136b52213928cbad27
Aramayis <mayissargsyan1995@gmail.com>
2021-05-11 18:28:12 +0400

    remove users with language equals to 'ru'

---
+++ Array_Method/Filter_02.js
@@ -0,0 +1,16 @@
+//- Write a function which remove users with language equals to 'ru'.
+const users = [
+  {
+    username: "Yuri Gagarin",
+    lang: "ru",
+  },
+  {
+    username: "Nil Armstrong",
+    lang: "ENG",
+  },
+];
+function filterUsers(arr) {
+  return arr.filter((el) => el.lang !== "ru");
+}
+
+console.log(filterUsers(users)); // [{ username: "Nil Armstrong, lang: "ENG" }]
