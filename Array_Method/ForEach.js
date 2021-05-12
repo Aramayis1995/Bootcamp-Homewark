509ec310f86c00da6eec8f9ca34d6309004bb368
Aramayis <mayissargsyan1995@gmail.com>
2021-05-12 22:18:50 +0400

    get ForEach

---
+++ Array_Method/ForEach.js
@@ -0,0 +1,11 @@
+function getForEach(arr, func) {
+  for (let i = 0; i < arr.length; i++) {
+    func(arr[i]);
+  }
+}
+
+//test
+function calculateSum(el) {
+  console.log(el);
+}
+getForEach([1, 2, 3, 4, 5], calculateSum);
