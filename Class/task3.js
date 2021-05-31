761865045583e4b446e1e970b0b375dad9f4b54f
Aramayis <mayissargsyan1995@gmail.com>
2021-05-31 22:57:24 +0400

    fix

--- Class/task3.js
+++ Class/task3.js
@@ -55,4 +55,77 @@ class Person {
 }
 let user1 = new Person("Poxos", "Poxosyan", "female", 23);
 user1.firstName = "Arthur";
-console.log(user1.toString()); // Name Surname, 23 years old.
+console.log(user1.toString());
+
+class Student extends Person {
+  constructor(firstName, lastName, gender, age, year, fee, program) {
+    super(firstName, lastName, gender, age);
+    this._year = year;
+    this._fee = fee;
+    this._program = program;
+  }
+  get year() {
+    return this._year;
+  }
+
+  set year(value) {
+    if (typeof value !== "number" || value > 100 || value < 0) {
+      throw Error("invalid year");
+    }
+    this._year = value;
+  }
+
+  get fee() {
+    return this._fee;
+  }
+
+  set fee(value) {
+    if (typeof value !== "number" || value < 0) {
+      throw Error("invalid fee");
+    }
+    this._fee = value;
+  }
+
+  get program() {
+    return this._program;
+  }
+
+  set program(value) {
+    if (!Array.isArray(value)) {
+      throw Error("invalid program");
+    }
+    this._program = value;
+  }
+
+  isAllPassed() {
+    return this.program.every((el) => el.grade > 50);
+  }
+
+  passExam(programName, grade) {
+    let index = this.program.findIndex((el) => el.programName === programName);
+    if (index === -1) {
+      return "invalid program";
+    } else {
+      this.program[index].grade = grade;
+    }
+    if (this.isAllPassed()) {
+      this.year++;
+    }
+  }
+}
+let arr = [
+  "Poxos",
+  "poxosyan",
+  "male",
+  10,
+  4,
+  100000,
+  [
+    { programName: "math", grade: 10 },
+    { programName: "english", grade: undefined },
+  ],
+];
+let Student1 = new Student(...arr);
+Student1.passExam("math", 60);
+Student1.passExam("english", 70);
+console.log(Student1.year);
